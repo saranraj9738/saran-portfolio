58df26b6c12cb9efeb5f1413f8305756434ef863
SaranRaj <saranraj9738@gmail.com>
2026-06-30 14:08:10 +0530

    Collateral Changes

--- src/App.jsx
+++ src/App.jsx
@@ -374,7 +374,7 @@ export default function App() {
 
                     <div className="mt-8">
                       <span className="px-4 py-2 bg-gray-100 rounded-full text-sm hover:scale-105 transition inline-block">
-                        Available for Freelance / Internships
+                        Available for opportunities
                       </span>
                     </div>
 
